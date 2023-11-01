4f016d32b7b1a58e455823a067666c6873b2ed29
jeffrey <jeffrey_jagram_90@hotmail.com>
2023-11-01 20:09:01 +0100

    Revert "sepolicy: qti: Redefine SEPOLICY_PLATFORM"
    This reverts commit 29ce88c83d138cb3f5af2f878181de8605e80565.

--- sepolicy/qti/SEPolicy.mk
+++ sepolicy/qti/SEPolicy.mk
@@ -3,8 +3,13 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-
-SEPOLICY_PLATFORM := $(subst device/qcom/sepolicy_vndr/,,$(SEPOLICY_PATH))
+ifeq ($(SEPOLICY_PATH), device/qcom/sepolicy_vndr/legacy-um)
+    SEPOLICY_PLATFORM := legacy-um
+else ifeq ($(SEPOLICY_PATH), device/qcom/sepolicy_vndr/sm8450)
+    SEPOLICY_PLATFORM := sm8450
+else ifeq ($(SEPOLICY_PATH), device/qcom/sepolicy_vndr/sm8550)
+    SEPOLICY_PLATFORM := sm8550
+endif
 
 BOARD_VENDOR_SEPOLICY_DIRS += \
     hardware/oplus/sepolicy/qti/vendor \
